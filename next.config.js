839e344cfa61e12979a5d52ab75c5ad46ee095a0
FuFuShIh <zx8714245@gmail.com>
2022-06-07 14:48:24 +0800

    Add setting to track wheather development or production.

--- next.config.js
+++ next.config.js
@@ -4,7 +4,10 @@ const nextConfig = {
   publicRuntimeConfig: {
     APP_NAME: 'SEOBLOG',
     API_DEVELOPMENT: 'http://localhost:8000/api',
-    PRODUCTION: false
+    API_PRODUCTION: 'https//seoblog.com',
+    PRODUCTION: false,
+    DOMAIN_DEVELOPMENT: 'http://localhost:3000',
+    DOMAIN_PRODUCTION: 'https//seoblog.com'
   }
 }
 
