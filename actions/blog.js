ee551ff549e19a456db3768a61caca5b03556fd4
FuFuShIh <zx8714245@gmail.com>
2022-06-09 14:55:43 +0800

    handling token expiry and redirect

--- actions/blog.js
+++ actions/blog.js
@@ -1,7 +1,7 @@
 import fetch from "isomorphic-fetch";
 import { API } from "../config";
 import queryString from 'query-string';
-import { isAuth } from "./auth";
+import { isAuth, handleResponse } from "./auth";
 
 export const createBlog = (blog, token) => {
   let createBlogEndpoint;
@@ -21,6 +21,7 @@ export const createBlog = (blog, token) => {
     body: blog
   })
     .then(res => {
+      handleResponse(res)
       return res.json();
     })
     .catch(err => console.log(err));
@@ -108,6 +109,7 @@ export const removeBlog = (slug, token) => {
     }
   })
     .then(response => {
+      handleResponse(response)
       return response.json()
     })
     .catch(err => console.log(err))
@@ -131,6 +133,7 @@ export const updateBlog = (blog, token, slug) => {
     body: blog
   })
     .then(res => {
+      handleResponse(res)
       return res.json();
     })
     .catch(err => console.log(err));
--- pages/signin.js
+++ pages/signin.js
@@ -1,11 +1,26 @@
 import React from 'react'
 import SigninComponent from '../components/auth/signin.component'
+import {withROuter} from 'next/router'
 import Layout from '../components/layout'
 
-const Signin = () => {
+const Signin = ({router}) => {
+  const showRedirectMessage = () => {
+    if(router.query.message) {
+      return (<div className='alert alert-danger'>{router.query.message}</div>)
+    } else {
+      return
+    }
+  }
+
   return (
     <Layout>
       <h2 className='text-center pt-4 pb-4'>Signin</h2>
+      <div className='row'>
+        <div className='col-md-6 offset-md-3'>
+          {showRedirectMessage()}
+        </div>
+      </div>
+
       <div className='row'>
         <div className='col-md-6 offset-md-3'>
           <SigninComponent />
@@ -15,4 +30,4 @@ const Signin = () => {
   )
 }
 
-export default Signin
+export default withROuter(Signin)
